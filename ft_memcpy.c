def11f6eb09c98a1cff73b4b030443c1b2e679ad
Gabriel Teixeira <pro.gabrielteixeira@gmail.com>
2021-12-16 12:33:07 +0000

    Teste

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: gateixei <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/01 18:26:46 by gateixei          #+#    #+#             */
-/*   Updated: 2021/12/14 19:17:56 by gateixei         ###   ########.fr       */
+/*   Updated: 2021/12/16 12:32:31 by gateixei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	int				c;
 
 	c = 0;
+	if (!dst)
+		return (0);
 	temp = (unsigned char *) dst;
 	while ((size_t) c < n)
 	{
