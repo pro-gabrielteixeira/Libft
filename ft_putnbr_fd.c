f65d5a3d4e9eac40c5467ec3c9b7a8f446a04188
Gabriel Teixeira araujo <gateixei@c1r15s15.42lisboa.com>
2022-03-07 18:40:15 +0000

    Mandatory 100% all tests

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: gateixei <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/03 22:26:39 by gateixei          #+#    #+#             */
-/*   Updated: 2022/03/03 23:02:44 by gateixei         ###   ########.fr       */
+/*   Updated: 2022/03/07 18:34:53 by gateixei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,18 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	if (n < -9 || n > 9)
-		ft_putnbr_fd((n / 10) , fd);
-	n = n % 10;
 	if (n < 0)
-		n = n * -1;
+	{
+		ft_putchar_fd('-', fd);
+		if (n == -2147483648)
+		{
+			ft_putstr_fd("2147483648", fd);
+			return ;
+		}
+		n = n * (-1);
+	}
+	if (n > 9)
+		ft_putnbr_fd((n / 10), fd);
+	n = n % 10;
 	ft_putchar_fd((n + '0'), fd);
 }
